5a7dd4def05530c9cdee77cb283203eddeb8163d
Makan Arastuie <makan.arastuie@gmail.com>
2019-02-20 17:22:42 -0500

    TF basics cover variables and placeholders

--- practice/tensorflow-tut.py
+++ practice/tensorflow-tut.py
@@ -154,3 +154,22 @@ sess2.close()
 
 ######################################################
 # PLACEHOLDERS
+
+# create a placeholder for a vector of 3 elements, type tf.float32
+a = tf.placeholder(tf.float32, shape=[3])
+
+b = tf.constant([5, 5, 5], tf.float32)
+
+# use the placeholder as you would a constant or a variable
+c = a + b  # short for tf.add(a, b)
+
+with tf.Session() as sess:
+    print(sess.run(c, feed_dict={a: [1, 2, 3]})) 	# the tensor a is the key, not the string ‘a’ # >> [6, 7, 8]
+
+# feeding in multiple points
+with tf.Session() as sess:
+    for a_value in []:
+        print(sess.run(c, {a: a_value}))
+
+
+# tf.Graph.is_feedable(tensor) #  True if and only if tensor is feedable.
